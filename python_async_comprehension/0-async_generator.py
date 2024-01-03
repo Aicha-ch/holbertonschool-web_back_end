4058da44a0ace3a46ada858b3c4b57731d95d0c0
Aicha-ch <chwikhi.aycha@gmail.com>
2024-01-03 16:53:27 +0100

    Async Generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+async generator
+"""
+
+
+import random
+import asyncio
+
+
+async def async_generator():
+    """ coroutine looping 10 times, wait 1 second
+    and then yield a random number between 0 and 10"""
+    for loop in range(10):
+        n = random.randint(0, 10)
+        await asyncio.sleep(1)
+        yield n
