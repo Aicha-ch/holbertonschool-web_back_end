078071ad03e5d8edc679ec1bb36759bef5881d5a
Aicha-ch <chwikhi.aycha@gmail.com>
2024-01-04 18:35:41 +0100

    update script

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -10,7 +10,3 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """Async Comprehensions"""
     return [num async for num in async_generator()]
-
-
-
-
