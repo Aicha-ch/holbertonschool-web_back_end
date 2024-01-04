b71d93b2c7e12d7f04957dbfc3993999e9fcddbd
Aicha-ch <chwikhi.aycha@gmail.com>
2024-01-04 17:34:49 +0100

    Async Comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -10,4 +10,5 @@ async_gen = __import__ ('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
 """ return 10 random numbers"""
- return [n async for n in async_generator()]
+return [n async for n in async_generator()]
+
