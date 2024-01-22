f90dd6c665fa754f565957cfa199d463a294c27f
Aicha-ch <chwikhi.aycha@gmail.com>
2024-01-22 12:04:55 +0100

    fix style errors

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const Arr = [];
-    for (const idx of array) {
-      Arr.push(appendString + idx);
-    }
-  
-    return Arr;
+  const Arr = [];
+  for (const idx of array) {
+    Arr.push(appendString + idx);
   }
+
+  return Arr;
+}
