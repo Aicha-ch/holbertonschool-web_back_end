efa6bc8b4151b58133c2c3169aab226c3d874464
Aicha-ch <chwikhi.aycha@gmail.com>
2024-01-22 12:07:06 +0100

    fix style errors

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,14 +1,14 @@
 export default function createReportObject(employeesList) {
-    const allEmployees = {};
-  
-    for (const [departmentName, employees] of Object.entries(employeesList)) {
-      allEmployees[departmentName] = [...employees];
-    }
-  
-    return {
-      allEmployees: allEmployees,
-      getNumberOfDepartments() {
-        return Object.keys(allEmployees).length;
-      },
-    };
+  const allEmployees = {};
+
+  for (const [departmentName, employees] of Object.entries(employeesList)) {
+    allEmployees[departmentName] = [...employees];
   }
+
+  return {
+    allEmployees,
+    getNumberOfDepartments() {
+      return Object.keys(allEmployees).length;
+    },
+  };
+}
